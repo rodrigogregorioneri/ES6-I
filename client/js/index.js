f022ace7831e29058cd18621aecfc31f90a49342
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-09-04 10:27:45 -0300

    adicionando valores na tabela

--- client/js/index.js
+++ client/js/index.js
@@ -1,11 +1,28 @@
-var campos= [
+var campos = [
     document.querySelector('#data'),
-    document.querySelector('#quantidade'),
-    document.querySelector('#valor')
-]
+    document.querySelector('#valor'), 
+    document.querySelector('#quantidade')
+];
 
-console.log(campos);
+var tbody = document.querySelector('table tbody');
 
-document.querySelector(".form").addEventListener('submit',function(event){
-       alert("Oi");
+document.querySelector('.form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
 });
